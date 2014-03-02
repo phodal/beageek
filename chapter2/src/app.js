83427919bb41413cb9a8b973ca1a2e416f69a7c2
Phodal <gmszone@qq.com>
2014-03-02 22:30:00 +0800

    simple Person object

--- chapter2/src/app.js
+++ chapter2/src/app.js
@@ -4,8 +4,8 @@ var Person=function(name,weight,height){
 	this.height=height;	
 	this.future=function(){
 		return "future";
-	}
-}
+	};
+};
 var phodal=new Person("phodal",50,166);
 document.write(phodal.name+"<br>");
 document.write(phodal.weight+"<br>");
